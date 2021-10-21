1ceb9e8596d1e9e8c414fcb43c16015d77ac9888
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-10-21 10:06:16 +0000

    Fixing some issues with the install

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
+echo "Copying zsh configurations..."
 cp zsh/krainboltgreene.zsh-theme ~/.oh-my-zsh/themes/krainboltgreene.zsh-theme
 cp zsh/.zshrc ~/.zshrc
-mkdir /tmp/gpg/
+echo "Setting up gpg..."
+mkdir -p /tmp/gpg/
 echo $GPG_PASSPHRASE > /tmp/gpg/passphrase
 echo $GPG_ARMOR_PAIR > /tmp/gpg/personal.armor
 gpg --passphrase-file /tmp/gpg/passphrase --import /tmp/gpg/personal.armor
-gpg --import-options restore --import gpg/publickeys.kbx
-mkdir ~/.ssh
+echo "Setting up ssh..."
+mkdir -p ~/.ssh/
 cp ssh/config ~/.ssh/config
-gpg --yes --passphrase-file /tmp/gpg/passphrase --output ~/.ssh/personal --decrypt ssh/personal
+gpg --yes --passphrase-file /tmp/gpg/passphrase --output ~/.ssh/personal --decrypt ssh/personal.enc
+chmod 600 ~/.ssh/personal
 cp ssh/personal.pub ~/.ssh/personal.pub
-ssh-add -K ~/.ssh/personal
+chmod 644 ~/.ssh/personal.pub
+eval `ssh-agent -s`
+echo $SSH_PASSPHRASE | SSH_ASKPASS=/bin/cat setsid -w ssh-add ~/.ssh/personal
+echo "Setting up git..."
 cp git/.gitconfig ~/.gitconfig
 cp git/.gitignore ~/.gitignore
 cp npm/.npmrc ~/.npmrc
