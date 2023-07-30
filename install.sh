280bac72159e822b9255c4af575195a1b30fdba0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2023-07-29 22:48:36 -0700

    Undoing ssh stuff for now

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ echo "Copying zsh configurations..."
 cp zsh/krainboltgreene.zsh-theme ~/.oh-my-zsh/themes/krainboltgreene.zsh-theme
 cp zsh/.zshrc ~/.zshrc
 echo "Setting up ssh..."
-mkdir -p ~/.ssh/
-cp ssh/config ~/.ssh/config
-cp ssh/personal.pub ~/.ssh/personal.pub
-chmod 644 ~/.ssh/personal.pub
+# mkdir -p ~/.ssh/
+# cp ssh/config ~/.ssh/config
+# cp ssh/personal.pub ~/.ssh/personal.pub
+# chmod 644 ~/.ssh/personal.pub
 echo "Setting up git..."
 cp git/.gitconfig ~/.gitconfig
 cp git/.gitignore ~/.gitignore
